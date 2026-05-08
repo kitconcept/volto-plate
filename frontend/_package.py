49458e695c99e7befaff31fab45a6a3eded63b21
Érico Andrei <ericof@gmail.com>
2026-05-08 15:12:25 -0300

    Update the release pipeline to also generate the artifact used by other projects.

---
+++ frontend/_package.py
@@ -0,0 +1,55 @@
+import subprocess
+from pathlib import Path
+
+PACKAGE_NAME = "volto-plate"
+cwd = Path(__file__).parent
+
+
+def versions_from_state(state) -> tuple[str, str]:
+    """Extract the current and previous version from the state."""
+    # this will be available in runtime
+    from repoplone.utils.versions import convert_python_node_version  # type: ignore
+
+    current_version = state["version"]
+    previous_version = state["previous_version"]
+    return (
+        convert_python_node_version(current_version),
+        convert_python_node_version(previous_version),
+    )
+
+
+def create_artifact(step_id, title, settings, state, **kwargs) -> bool:
+    """Create the artifact for the newly created version."""
+    artifacts_dir = cwd / "artifacts"
+    current_version, previous_version = versions_from_state(state)
+    if not artifacts_dir.exists():
+        artifacts_dir.mkdir(parents=True)
+    artifact_file = artifacts_dir / f"kitconcept-{PACKAGE_NAME}-{current_version}.tgz"
+    cmd = ["pnpm", "run", "artifact-release"]
+    result = subprocess.run(  # noQA: S602
+        " ".join(cmd),
+        capture_output=True,
+        text=True,
+        shell=True,
+        cwd=cwd,
+    )
+    if result.returncode:
+        raise RuntimeError(f"Create artifact failed {result.stderr}")
+    elif not artifact_file.exists():
+        raise RuntimeError(
+            f"Artifact file {artifact_file} not found after creation. {result.stdout}"
+        )
+    # Cleanup (Remove old artifact if exists)
+    previous_artifact_file = (
+        artifacts_dir / f"kitconcept-{PACKAGE_NAME}-{previous_version}.tgz"
+    )
+    final_file = artifacts_dir / f"kitconcept-{PACKAGE_NAME}.tgz"
+    # Remove artifact for previous version if exists
+    if previous_artifact_file.exists():
+        previous_artifact_file.unlink()
+    # Remove existing artifact if exists
+    if final_file.exists():
+        final_file.unlink()
+    # Rename artifact
+    artifact_file.rename(final_file)
+    return True
