10d3222b798f2b1ad59850e81a2763ff248c41a1
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-05-11 17:38:54 +0200

    REmove CSS hack to neutralize the .container from Tailwind

--- frontend/packages/volto-plate/src/config/settings.ts
+++ frontend/packages/volto-plate/src/config/settings.ts
@@ -3,5 +3,14 @@ import type { ConfigType } from '@plone/registry';
 export default function install(config: ConfigType) {
   config.settings.PlateEditorContentTypes = ['WikiPage'];
 
+  config.settings.cssLayers = [
+    'properties',
+    'theme',
+    'base',
+    'components',
+    'utilities',
+    'plone-components',
+  ];
+
   return config;
 }
